5e73fe7a7b33b62a8f6c3cc6ad29dc7fcf59607d
wanarekapil96@ <wanarekapil96@gmail.com>
2023-03-16 19:22:07 +0530

    It Will Shows The Employee Wage Computation Problem For A Month

--- Employee-Wage-Computation.java
+++ Employee-Wage-Computation.java
@@ -4,6 +4,7 @@ public class Employee-Wage-Computation {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
 
     public static void main(String args[])
     {
@@ -11,21 +12,27 @@ public class Employee-Wage-Computation {
         // Variable Definitions
         int empHrs = 0;
         int empWage = 0;
+        int totalEmpWage = 0;
         // Operation
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        switch ((int)empCheck)
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
         {
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            default:
-                empHrs = 0;
-                break;
+            double empCheck = Math.floor(Math.random() * 10) % 3;
+            switch ((int)empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
+            System.out.println("Emp Wage: " + empWage);
         }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Emp Wage: " + empWage);
+        System.out.println("Total Emp Wage: " + totalEmpWage);
     }
 }
