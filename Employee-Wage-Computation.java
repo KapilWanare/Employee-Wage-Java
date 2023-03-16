e270ee4ea10f31fd14a8248babce76ce7bed1922
wanarekapil96@ <wanarekapil96@gmail.com>
2023-03-16 19:05:59 +0530

    It Will Shows The Employee Wage Part Time

--- Employee-Wage-Computation.java
+++ Employee-Wage-Computation.java
@@ -1,10 +1,10 @@
-import java.util.*;
-class Employee-Wage-Computation
-{
-    public static void main(String args[])
-    {
+public class  Employee-Wage-Computation
+        {
+public static void main(String args[])
+        {
         System.out.println("Welcome to Employee Wage Computation Program!");
         // Constants Definitions
+        int IS_PART_TIME = 1;
         int IS_FULL_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
         // Variable Definitions
@@ -12,11 +12,14 @@ class Employee-Wage-Computation
         int empWage = 0;
         // Operation
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if(empCheck == IS_FULL_TIME)
-            empHrs = 8;
+        if(empCheck == IS_PART_TIME)
+        empHrs = 4;
+        else if(empCheck == IS_FULL_TIME)
+        empHrs = 8;
         else
-            empHrs = 0;
+        empHrs = 0;
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Emp Wage: " + empWage);
-    }
-}
+        }
+        }
+
