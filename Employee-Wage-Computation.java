328994ddddb5c2e3ac5ad492db465dc5c3da40c5
wanarekapil96@ <wanarekapil96@gmail.com>
2023-03-16 19:44:50 +0530

    It Will Shows The Employee Wage for multiple companies - Note: Each Company has its own wage, number of working days and working hours per month - Use Class Method with function parameters instead of Class Variables

--- Employee-Wage-Computation.java
+++ Employee-Wage-Computation.java
@@ -2,8 +2,13 @@ import java.util.*;
 class Employee-Wage-Computation
 {
     // Constants Definitions
-    public static final int IS_PART_TIME = 1;
-    public static final int IS_FULL_TIME = 2;
+    private static final int IS_PART_TIME=1, IS_FULL_TIME=2;
+    private static String COMPANY_NAME;
+
+    public UC8(String companyName)
+    {
+        this.COMPANY_NAME=companyName;
+    }
 
     private int calculateWage(	int employeeID,
                                   int partTimeHrs,
@@ -13,8 +18,8 @@ class Employee-Wage-Computation
                                   int hoursWorked	)
     {
         // Function description & welcome message
-        System.out.println("Welcome to Employee Wage Computation Program!");
-
+        System.out.println("**********\tWelcome to Employee Wage Computation Program!\t**********");
+        System.out.println("**********\tComputing for company: "+this.COMPANY_NAME+"\t**********");
         // Variable Definitions
         int empHrs = 0;
         int totalEmpHrs = 0;
@@ -39,7 +44,7 @@ class Employee-Wage-Computation
                     break;
             }
             totalEmpHrs += empHrs;
-            System.out.println("\tDay#: "+totalWorkingDays+"\tHours woked today: "+empHrs+"\tToday's Wage: "+empHrs*wagePerHour);
+            System.out.println("Day#: "+totalWorkingDays+"\t\tHours woked today: "+empHrs+"\tToday's Wage: "+empHrs*wagePerHour);
         }
         totalEmpWage = totalEmpHrs * wagePerHour;
         System.out.println("Employee with eID#"+employeeID+" got total monthly wage of "+totalEmpWage+" Rs by working "+totalWorkingDays+" day(s) totalling "+totalEmpHrs+" hour(s).");
@@ -47,7 +52,7 @@ class Employee-Wage-Computation
         return totalEmpWage;
     }
 
-    public static void main(String[] args)
+    public void readEmpToGetWage()
     {
         Scanner sc = new Scanner(System.in);
 
@@ -69,9 +74,17 @@ class Employee-Wage-Computation
         System.out.print("Enter number of hours employee worked: ");
         int hoursWorked=sc.nextInt();
 
-        UC7 Alice = new UC7();
-        Alice.calculateWage(employeeID, partTimeHrs, fullTimeHrs, wagePerHour, daysWorked, hoursWorked);
-
         sc.close();
+
+        int employeeWage = calculateWage(employeeID, partTimeHrs, fullTimeHrs, wagePerHour, daysWorked, hoursWorked);
+    }
+
+    public static void main(String[] args)
+    {
+        UC8 DMart = new UC8("DMart");
+        DMart.readEmpToGetWage();
+
+        // ComputeEmployeeWage Walmart = new ComputeEmployeeWage("Walmart");
+        // Walmart.readEmpToGetWage();
     }
 }
