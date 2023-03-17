7b0bf40c735e98120525b98ccc45c38529c0e090
wanarekapil96@ <wanarekapil96@gmail.com>
2023-03-17 20:45:47 +0530

    It Will Shows The Employee Wage Ability to manageEmployee Wage of multiple companies

--- Employee-Wage-Computation.java
+++ Employee-Wage-Computation.java
@@ -1,5 +1,4 @@
-
-public class Employee-Wage-Computation {
+public class EmployeeWage {
 
     static final int IS_FULL_TIME = 1;
     static final int IS_PART_TIME = 2;
@@ -9,10 +8,7 @@ public class Employee-Wage-Computation {
     private int WORKING_DAYS_PER_MONTH ;
 
     private int TOTAL_EMPLOYEE_WAGE ;
-
-
-
-    public Employee-Wage-Computation() {
+    public EmployeeWage() {
     }
 
     public EmployeeWage(int EMP_WAGE_PER_HOUR, int FULL_TIME_HOUR, int PART_TIME_HOUR, int WORKING_DAYS_PER_MONTH) {
@@ -21,36 +17,27 @@ public class Employee-Wage-Computation {
         this.PART_TIME_HOUR = PART_TIME_HOUR;
         this.WORKING_DAYS_PER_MONTH = WORKING_DAYS_PER_MONTH;
     }
-
     public void calculateEmployeeWage() {
-
         int empHours = 0 ;
         int day = 1;
         int totalWage = 0;
         while (day < WORKING_DAYS_PER_MONTH && empHours <= 100) {
             int dailyWage = 0;
             int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
-            System.out.println(employeeCheck);
             switch (employeeCheck) {
                 case IS_FULL_TIME:
-                    System.out.println("Employee is present");
                     dailyWage = EMP_WAGE_PER_HOUR * FULL_TIME_HOUR;
                     empHours += FULL_TIME_HOUR ;
-
                     break;
                 case IS_PART_TIME:
-                    System.out.println("Employee is present");
                     dailyWage = EMP_WAGE_PER_HOUR * PART_TIME_HOUR;
                     empHours += PART_TIME_HOUR ;
 
                     break;
                 default:
-                    System.out.println("Employee is absent");
             }
             day++;
             totalWage += dailyWage ;
-            System.out.println("Total employee hours are " + empHours);
-            System.out.println("Total wage for " + day + " day is " + totalWage);
         }
         TOTAL_EMPLOYEE_WAGE = totalWage;
 
@@ -58,16 +45,22 @@ public class Employee-Wage-Computation {
 
     @Override
     public String toString() {
-        return "total employee wage = " + TOTAL_EMPLOYEE_WAGE ;
+        return "total employee wage is = " + TOTAL_EMPLOYEE_WAGE ;
     }
 
     public static void main(String[] args) {
-        Employee-Wage-Computation perfios  = new Employee-Wage-Computation(35,8,5,23);
-        perfios.calculateEmployeeWage();
-        System.out.println("Perfios " + perfios.toString());
-        Employee-Wage-Computation reliance = new Employee-Wage-Computation(30,14,7,24);
-        reliance.calculateEmployeeWage();
-        System.out.println("Reliance " + reliance.toString());
-
+        EmployeeWage[] employeeWages = new EmployeeWage[3];
+        EmployeeWage wipro = new EmployeeWage(15,12,7,24);
+        EmployeeWage tcs = new EmployeeWage(20,13,7,22);
+        EmployeeWage google = new EmployeeWage(30,11,6,20);
+        employeeWages[0] = wipro;
+        employeeWages[1] = tcs;
+        employeeWages[2] = google;
+        employeeWages[0].calculateEmployeeWage();
+        employeeWages[1].calculateEmployeeWage();
+        employeeWages[2].calculateEmployeeWage();
+        for (int i =0;i<employeeWages.length;i++){
+            System.out.println(employeeWages[i]);
+        }
     }
 }
